c666bc41d5db2bb32588e9071f9ecc953b99841c
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-29 17:10:47 -0700

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin('docs/bundle.js')
+  ]
 }
