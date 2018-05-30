b92896912ba332fc5d45e1344ce4220c0a09d44a
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-30 05:44:34 -0700

    set up header text

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  // I installed the class properties Babel plugin, but here's another way to set things up:
+
+  // constructor() {
+  //  super();
+  //  this.state = {
+  //    header: 'HEADER TEXT'
+  //  };
+  // }
+
+  state = {
+    header: 'HEADER TEXT'
+  };
+
+  handleHeaderChange({ target }) {
+    this.setState({ header: target.value });
+  }
+  
   render() {
+    const { header } = this.state;
     return (
       <main>
-        <h1>Oh hey, world!</h1>
+        <section className="meme">
+          <h1>Your Meme</h1>
+          <h2>{header}</h2>
+        </section>
+
+        <section className="controls">
+          <label>
+          Enter a header for your meme:
+            <input
+              type="text"
+              value={header}
+              onChange={event => this.handleHeaderChange(event)}
+            />
+          </label>
+        </section>
       </main>
     );
   }
