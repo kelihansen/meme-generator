a3166cbfa830abcde65ace2b6f5392159bc2041a
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-30 05:54:58 -0700

    set up footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  // I installed the class properties Babel plugin, but here's another way to set things up:
+  // I installed the "class properties" Babel plugin, but here's another way to set things up:
 
   // constructor() {
   //  super();
@@ -11,20 +11,26 @@ export default class App extends Component {
   // }
 
   state = {
-    header: 'HEADER TEXT'
+    header: 'TEXT',
+    footer: 'TEXT'
   };
 
   handleHeaderChange({ target }) {
     this.setState({ header: target.value });
   }
+
+  handleHooterChange({ target }) {
+    this.setState({ footer: target.value });
+  }
   
   render() {
-    const { header } = this.state;
+    const { header, footer } = this.state;
     return (
       <main>
         <section className="meme">
           <h1>Your Meme</h1>
           <h2>{header}</h2>
+          <h2>{footer}</h2>
         </section>
 
         <section className="controls">
@@ -34,6 +40,14 @@ export default class App extends Component {
               type="text"
               value={header}
               onChange={event => this.handleHeaderChange(event)}
+            />
+          </label>
+          <label>
+          Enter a footer for your meme:
+            <input
+              type="text"
+              value={footer}
+              onChange={event => this.handleFooterChange(event)}
             />
           </label>
         </section>
